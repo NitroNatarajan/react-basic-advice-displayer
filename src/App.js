bcf3b2062796069a2dc86db3935e056a81098121
NitroNatarajan <natarajan05011994@gmail.com>
2023-06-06 16:40:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react";
 function App() {
+  const[advice,setAdvice]= useState("");
+  const[count,setCount]= useState(0);
+
+ async function getAdvice(){
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  setAdvice(data.slip.advice);
+  setCount((c)=>c+1)
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count}/>
     </div>
   );
 }
 
+function Message (props){
+  return (<div>
+    <p>You have read <strong>{props.count} advices</strong></p>
+ </div>
+ )  
+}
 export default App;
